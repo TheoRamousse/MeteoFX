07127dde80734bca380ce86000a1c36dc6fafc7c
thramousse <theo.ramousse@etu.uca.fr>
2019-11-23 19:52:12 +0100

    Création de l'apprence générale de la fenêtre principale mainView

---
+++ meteoFX/src/controllers/MainView.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.fxml.FXML;
+
+public class MainView {
+}
+
+@FXML
+public void initialize()
+{
+
+}
--- meteoFX/src/launcher/Main.java
+++ meteoFX/src/launcher/Main.java
@@ -13,15 +13,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainView.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        new SensorTest().testRandomCensor();
         launch(args);
+        new SensorTest().testRandomCensor();
     }
 }
